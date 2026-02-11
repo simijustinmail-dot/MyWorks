27a224551b65511d3395c36ab720b58c2452fcb1
simijustinmail-dot <simijustin.mail@gmail.com>
2026-02-11 16:50:08 +0530

    Add files via upload

--- src/pages/AssetDashboard.js
+++ src/pages/AssetDashboard.js
@@ -1,27 +1,37 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
-import MainLayout from '../layouts/Mainlayout'
+import MainLayout from '../layouts/Mainlayout';
+import { UserContext } from "../context/UserContext";
+
 
 const actions = [
-  { name: "Add Asset Type", icon: "bi-folder-plus", route: "/assets/add-assettype" },//route: "/assets/add-subtype" },
-  { name: "Add Asset SubType", icon: "bi-folder2-open", route: "/assets/add-assetsubtype" },//route: "/assets/add-subtypeentity" },
-  { name: "Add Asset SubTypeEntity", icon: "bi-tags", route: "/assets/add-assetsubtypeentity" },//route: "/assets/add-subtypeentitynodes" },
-  { name: "Add Asset SubTypeEntityNode", icon: "bi-diagram-3", route: "/assets/add-assetsubtypeentitynode" },//route: "/assets/add-subtypeentitynodes" },
-  { name: "Add Asset", icon: "bi-box-seam", route: "/assets/add-asset" },//route: "/assets/add-asset" },
-  { name: "List Asset", icon: "bi-table", route: "/assets/list-asset" },//route: "/assets/add-asset" },
+  { name: "Add Asset Type", icon: "bi-folder-plus", route: "/assets/add-assettype" , color: "primary" },//route: "/assets/add-subtype" },
+  { name: "Add Asset SubType", icon: "bi-folder2-open", route: "/assets/add-assetsubtype" , color: "primary" },//route: "/assets/add-subtypeentity" },
+  { name: "Add Asset SubTypeEntity", icon: "bi-tags", route: "/assets/add-assetsubtypeentity" , color: "primary" },//route: "/assets/add-subtypeentitynodes" },
+  { name: "Add Asset SubTypeEntityNode", icon: "bi-diagram-3", route: "/assets/add-assetsubtypeentitynode" , color: "primary" },//route: "/assets/add-subtypeentitynodes" },
+  { name: "Add Asset", icon: "bi-box-seam", route: "/assets/add-asset" , color: "success" ,  excludeRoles: ["SectionAdmin"]},//route: "/assets/add-asset" },
+  { name: "List Asset", icon: "bi-table", route: "/assets/list-asset" , color: "secondary" },//route: "/assets/add-asset" },
+  { name: "Bulk Update Asset", icon: "bi-layers", route: "/assets/bulkupdate-asset" , color: "warning" ,  excludeRoles: ["SectionAdmin"] }
 ];
-const cardColors = ['primary', 'success', 'info', 'warning', 'danger', 'secondary'];
+
+//const cardColors = ['primary', 'success', 'info', 'warning', 'danger', 'secondary'];
 const AssetDashboard = () => {
+  const { user } = useContext(UserContext);
+  const userRole = user?.role;
+  const visibleActions = actions.filter(action => { //hide menu for excludedroles for ex: sectionadmin wont see bulk update menu
+    if (!action.excludeRoles) return true;
+    return !action.excludeRoles.includes(userRole);
+  });
   const navigate = useNavigate();
   return (
     <MainLayout>
       <div className="container py-4">
         <h3 className="mb-4 text-primary">Asset Dashboard</h3>
         <div className="row">
-          {actions.map((action, i) => (
+          {visibleActions.map((action, i) => (
             <div className="col-md-4 mb-4" key={i}>
               <div
-                className={`card text-center text-white bg-${cardColors[i % cardColors.length]} shadow h-100`}
+                className={`card text-center text-white bg-${action.color} shadow h-100`}
                 style={{ cursor: 'pointer' }}
                 onClick={() => navigate(action.route)}
               >
